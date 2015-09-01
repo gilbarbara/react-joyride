bf7d33133740d990a26afb37c9586222883e35c2
Gil Barbara <gilbarbara@gmail.com>
2015-09-01 19:20:22 -0300

    daily fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,16 @@ var gulp                  = require('gulp'),
     $                     = require('gulp-load-plugins')(),
     del                   = require('del'),
     merge                 = require('merge-stream'),
-    runSequence           = require('run-sequence'),
-    AUTOPREFIXER_BROWSERS = [
-        'ie >= 10',
-        'ie_mob >= 10',
-        'ff >= 30',
-        'chrome >= 34',
-        'safari >= 7',
-        'opera >= 23',
-        'ios >= 7',
-        'android >= 4.4',
-        'bb >= 10'
-    ];
+    runSequence           = require('run-sequence');
 
 // Script
 gulp.task('lint', function () {
     return gulp.src('lib/scripts/*')
         .pipe($.eslint({
-            useEslintrc: true
+            useEslintrc: true,
+            rules: {
+                "no-console": 2
+            }
         }))
         .pipe($.eslint.format())
         .pipe($.eslint.failOnError());
@@ -32,9 +24,7 @@ gulp.task('styles', function () {
             precision: 4
         }).on('error', $.sass.logError))
         .pipe($.plumber.stop())
-        .pipe($.autoprefixer({
-            browsers: ['last 4 versions']
-        }))
+        .pipe($.autoprefixer())
         .pipe(gulp.dest('lib/styles'));
 });
 
@@ -44,8 +34,7 @@ gulp.task('clean', function (cb) {
 
 gulp.task('build', function (cb) {
     process.env.NODE_ENV = 'production';
-    runSequence('clean', 'lint', 'readme', 'assets', ['media', 'bundle'], 'sizer', cb);
+    runSequence('clean', 'lint', 'styles', cb);
 });
 
-gulp.task('default', ['serve']);
-
+gulp.task('default', ['build']);
