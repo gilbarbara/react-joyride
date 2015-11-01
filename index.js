ad06f0ba51f42e507373b3d09f4049c495b676cf
Gil Barbara <gilbarbara@gmail.com>
2015-11-01 20:05:37 -0200

    remove Mixin

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = {
-    Joyride: require('./lib/scripts/Joyride.js'),
-    Mixin: require('./lib/scripts/Mixin.js'),
-    Beacon: require('./lib/scripts/Beacon.js'),
-    Tooltip: require('./lib/scripts/Tooltip.js')
+    Component: require('./lib/scripts/Component.js'),
+    Mixin: require('./lib/scripts/Mixin.js')
 };
