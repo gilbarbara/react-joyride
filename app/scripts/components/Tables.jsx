655532ea99ce08a559c23507177e0cf6d9f7c57c
Gil Barbara <gilbarbara@gmail.com>
2016-07-08 02:22:38 -0300

    add a new step to Tables inside a setTimout

--- app/scripts/components/Tables.jsx
+++ app/scripts/components/Tables.jsx
@@ -19,125 +19,168 @@ class Tables extends React.Component {
         }
       }
     });
+
+    setTimeout(() => {
+      this.props.addSteps({
+        title: 'Add steps after it has started',
+        text: 'This step was added 2 seconds later in a setTimeout call',
+        selector: '.tasks',
+        position: 'right'
+      });
+    }, 3000);
   }
 
   render() {
     return (
-      <div className="row tables">
-        <div className="col-xs-12 col-md-6">
-          <div className="card">
-            <div className="card-header">
-              <h3>
-                <i className="fa fa-clock-o fa-fw" /> Tasks Panel
-              </h3>
-            </div>
-            <div className="card-block">
-              <ul className="list-group">
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">just now</span>
-                  <i className="fa fa-fw fa-calendar" /> Calendar updated
-                </li>
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">4 minutes ago</span>
-                  <i className="fa fa-fw fa-comment" /> Commented on a post
-                </li>
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">23 minutes ago</span>
-                  <i className="fa fa-fw fa-truck" /> Order 392 shipped
-                </li>
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">46 minutes ago</span>
-                  <i className="fa fa-fw fa-money" /> Invoice 653 has been paid
-                </li>
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">1 hour ago</span>
-                  <i className="fa fa-fw fa-user" /> A new user has been added
-                </li>
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">2 hours ago</span>
-                  <i className="fa fa-fw fa-check" /> Completed task: "pick up dry cleaning"
-                </li>
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">yesterday</span>
-                  <i className="fa fa-fw fa-globe" /> Saved the world
-                </li>
-                <li className="list-group-item">
-                  <span className="label label-pill label-default pull-right">two days ago</span>
-                  <i className="fa fa-fw fa-check" /> Completed task: "fix error on sales page"
-                </li>
-              </ul>
-            </div>
-            <div className="card-footer">
-              <div className="text-right">
-                <a href="#">View All Activity <i
-                  className="fa fa-arrow-circle-right" /></a>
+      <div className="tables">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            <div className="card tasks">
+              <div className="card-header">
+                <h3>
+                  <i className="fa fa-clock-o fa-fw" /> Tasks Panel
+                </h3>
+              </div>
+              <div className="card-block">
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">just now</span>
+                    <i className="fa fa-fw fa-calendar" /> Calendar updated
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">4 minutes ago</span>
+                    <i className="fa fa-fw fa-comment" /> Commented on a post
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">23 minutes ago</span>
+                    <i className="fa fa-fw fa-truck" /> Order 392 shipped
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">46 minutes ago</span>
+                    <i className="fa fa-fw fa-money" /> Invoice 653 has been paid
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">1 hour ago</span>
+                    <i className="fa fa-fw fa-user" /> A new user has been added
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">2 hours ago</span>
+                    <i className="fa fa-fw fa-check" /> Completed task: "pick up dry cleaning"
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">yesterday</span>
+                    <i className="fa fa-fw fa-globe" /> Saved the world
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">two days ago</span>
+                    <i className="fa fa-fw fa-check" /> Completed task: "fix error on sales page"
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">just now</span>
+                    <i className="fa fa-fw fa-calendar" /> Calendar updated
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">4 minutes ago</span>
+                    <i className="fa fa-fw fa-comment" /> Commented on a post
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">23 minutes ago</span>
+                    <i className="fa fa-fw fa-truck" /> Order 392 shipped
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">46 minutes ago</span>
+                    <i className="fa fa-fw fa-money" /> Invoice 653 has been paid
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">1 hour ago</span>
+                    <i className="fa fa-fw fa-user" /> A new user has been added
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">2 hours ago</span>
+                    <i className="fa fa-fw fa-check" /> Completed task: "pick up dry cleaning"
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">yesterday</span>
+                    <i className="fa fa-fw fa-globe" /> Saved the world
+                  </li>
+                  <li className="list-group-item">
+                    <span className="label label-pill label-default pull-right">two days ago</span>
+                    <i className="fa fa-fw fa-check" /> Completed task: "fix error on sales page"
+                  </li>
+                </ul>
+              </div>
+              <div className="card-footer">
+                <div className="text-right">
+                  <a href="#">View All Activity <i
+                    className="fa fa-arrow-circle-right" /></a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-xs-12 col-md-6">
-          <div ref="transactions" className="card transactions">
-            <div className="card-header">
-              <h3>
-                <i className="fa fa-money fa-fw" /> Transactions Panel</h3>
-            </div>
-            <div className="card-block">
-              <div className="table-responsive">
-                <table className="table table-bordered table-hover table-striped">
-                  <thead>
-                  <tr>
-                    <th>Order #</th>
-                    <th>Order Date</th>
-                    <th>Order Time</th>
-                    <th>Amount (USD)</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>3326</td>
-                    <td>08/21/2015</td>
-                    <td>3:29 PM</td>
-                    <td>$321.33</td>
-                  </tr>
-                  <tr>
-                    <td>3325</td>
-                    <td>08/21/2015</td>
-                    <td>3:20 PM</td>
-                    <td>$234.34</td>
-                  </tr>
-                  <tr>
-                    <td>3324</td>
-                    <td>08/21/2015</td>
-                    <td>3:03 PM</td>
-                    <td>$724.17</td>
-                  </tr>
-                  <tr>
-                    <td>3323</td>
-                    <td>08/21/2015</td>
-                    <td>3:00 PM</td>
-                    <td>$23.71</td>
-                  </tr>
-                  <tr>
-                    <td>3322</td>
-                    <td>08/21/2015</td>
-                    <td>2:49 PM</td>
-                    <td>$8345.23</td>
-                  </tr>
-                  <tr>
-                    <td>3321</td>
-                    <td>08/21/2015</td>
-                    <td>2:23 PM</td>
-                    <td>$245.12</td>
-                  </tr>
-                  </tbody>
-                </table>
+          <div className="col-xs-12 col-md-6">
+            <div ref="transactions" className="card transactions">
+              <div className="card-header">
+                <h3>
+                  <i className="fa fa-money fa-fw" /> Transactions Panel</h3>
               </div>
-            </div>
+              <div className="card-block">
+                <div className="table-responsive">
+                  <table className="table table-bordered table-hover table-striped">
+                    <thead>
+                    <tr>
+                      <th>Order #</th>
+                      <th>Order Date</th>
+                      <th>Order Time</th>
+                      <th>Amount (USD)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                      <td>3326</td>
+                      <td>08/21/2015</td>
+                      <td>3:29 PM</td>
+                      <td>$321.33</td>
+                    </tr>
+                    <tr>
+                      <td>3325</td>
+                      <td>08/21/2015</td>
+                      <td>3:20 PM</td>
+                      <td>$234.34</td>
+                    </tr>
+                    <tr>
+                      <td>3324</td>
+                      <td>08/21/2015</td>
+                      <td>3:03 PM</td>
+                      <td>$724.17</td>
+                    </tr>
+                    <tr>
+                      <td>3323</td>
+                      <td>08/21/2015</td>
+                      <td>3:00 PM</td>
+                      <td>$23.71</td>
+                    </tr>
+                    <tr>
+                      <td>3322</td>
+                      <td>08/21/2015</td>
+                      <td>2:49 PM</td>
+                      <td>$8345.23</td>
+                    </tr>
+                    <tr>
+                      <td>3321</td>
+                      <td>08/21/2015</td>
+                      <td>2:23 PM</td>
+                      <td>$245.12</td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
 
-            <div className="card-footer">
-              <div className="text-right">
-                <a href="#">View All Transactions <i
-                  className="fa fa-arrow-circle-right" /></a>
+              <div className="card-footer">
+                <div className="text-right">
+                  <a href="#">View All Transactions <i
+                    className="fa fa-arrow-circle-right" /></a>
+                </div>
               </div>
             </div>
           </div>
