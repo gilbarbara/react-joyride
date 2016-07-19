b0fc35bd76c5bd17d9b4e1b2c3e7f8bbbd60e792
Gil Barbara <gilbarbara@gmail.com>
2016-07-19 00:41:58 -0300

    update tests to use saucelabs

--- test/nightwatch.conf.js
+++ test/nightwatch.conf.js
@@ -2,21 +2,27 @@
 var jar = require('selenium-server-standalone-jar');
 var chromedriver = require('chromedriver');
 
+const TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER;
+
 module.exports = {
   src_folders: ['test'],
   output_folder: 'reports',
   selenium: {
-    start_process: true,
+    start_process: false,
     server_path: jar.path,
     cli_args: {
       'webdriver.chrome.driver': chromedriver.path,
       'webdriver.ie.driver': ''
     }
   },
-  test_runner: { type: 'mocha', options: { bail: true } },
+  test_runner: { type: 'mocha', options: { bail: 'bail' } },
   test_settings: {
     default: {
-      launch_url: 'http://localhost',
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      selenium_host: 'ondemand.saucelabs.com',
+      selenium_port: 80,
+      username: process.env.SAUCE_USERNAME,
+      access_key: process.env.SAUCE_ACCESS_KEY,
       filter: 'test/*.nightwatch.js',
       screenshots: {
         enabled: false,
@@ -24,9 +30,12 @@ module.exports = {
       },
       desiredCapabilities: {
         browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
+        platform: 'OS X 10.11',
+        version: '51.0',
+        build: `build-${TRAVIS_JOB_NUMBER}`,
+        'tunnel-identifier': TRAVIS_JOB_NUMBER,
+        screenResolution: '1280x960'
+      },
     },
     firefox: {
       desiredCapabilities: {
