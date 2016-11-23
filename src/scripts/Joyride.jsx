9711f9a8333f074af845950d9846553a7072f7dc
Maureen Fortin <xveganxxxedgex@gmail.com>
2016-11-23 14:18:54 -0800

    [master] more logs

--- src/scripts/Joyride.jsx
+++ src/scripts/Joyride.jsx
@@ -437,7 +437,7 @@ export default class Joyride extends React.Component {
     let scrollTo = 0;
     /* eslint-disable no-console */
     console.warn(targetTop, useScrollContainer, this.getScrollContainer(useScrollContainer).scrollTop, scrollOffset);
-    console.warn(target.getBoundingClientRect(), this.getScrollContainer(useScrollContainer).getBoundingClientRect(), target.offsetTop);
+    console.warn(target.offsetTop, this.getScrollContainer(useScrollContainer).height);
     /* eslint-enable no-console */
 
     if (/^top/.test(position)) {
