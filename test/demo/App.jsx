7ae72c201c1f77413c21ce3fa9dd7c11a65f42aa
Maureen Fortin <xveganxxxedgex@gmail.com>
2017-01-10 10:51:43 -0800

    [scroll-containers] add step to show bug disabling events when tooltip overlaps target hole

--- test/demo/App.jsx
+++ test/demo/App.jsx
@@ -9,6 +9,13 @@ export default class Demo extends React.Component {
       running: false,
       steps: [
         {
+          title: 'Target Body',
+          text: 'Events become disabled when large tooltip overlaps target hole :(',
+          textAlign: 'center',
+          selector: '.hero',
+          position: 'top'
+        },
+        {
           title: 'Our Projects',
           text: 'Ooops. I forgot to add images!',
           textAlign: 'center',
