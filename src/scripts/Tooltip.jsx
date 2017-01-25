230887ebc8c23d9b5eba9d1098b46de86319f69d
stouch <syl20.touchard@gmail.com>
2017-01-25 14:51:04 +0100

    Fixes horizontal hole position calculation for fixed elements

--- src/scripts/Tooltip.jsx
+++ src/scripts/Tooltip.jsx
@@ -357,8 +357,9 @@ export default class JoyrideTooltip extends React.Component {
     const event = e || window.e;
     const hole = this.state.styles.hole;
     const offsetY = hole.position === 'fixed' ? event.clientY : event.pageY;
+    const offsetX = hole.position === 'fixed' ? event.clientX : event.pageX;
     const inHoleHeight = (offsetY >= hole.top && offsetY <= hole.top + hole.height);
-    const inHoleWidth = (event.pageX >= hole.left && event.pageX <= hole.left + hole.width);
+    const inHoleWidth = (offsetX >= hole.left && offsetX <= hole.left + hole.width);
     const inHole = inHoleWidth && inHoleHeight;
     if (inHole && !this.state.mouseOverHole) {
       this.setState({ mouseOverHole: true });
