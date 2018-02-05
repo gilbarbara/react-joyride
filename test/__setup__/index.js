c10ea3bd7065f8717bf7efe2d1831068af18629e
Gil Barbara <gilbarbara@gmail.com>
2018-02-05 20:56:07 -0200

    upgrade dependencies

--- test/__setup__/index.js
+++ test/__setup__/index.js
@@ -5,26 +5,6 @@ import './polyfills';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-Object.defineProperty(window.location, 'href', {
-  writable: true,
-  value: 'http://localhost:3000/',
-});
-
-Object.defineProperty(window.location, 'pathname', {
-  writable: true,
-  value: '/',
-});
-
-Object.defineProperty(window.location, 'search', {
-  writable: true,
-  value: '',
-});
-
-Object.defineProperty(window, 'open', {
-  writable: true,
-  value: '',
-});
-
 Object.defineProperty(Element.prototype, 'clientHeight', {
   writable: true,
   value: '',
