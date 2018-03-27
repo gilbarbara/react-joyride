b4e63d4dbdbfd33067a2607b688cb3d9820e1744
Gil Barbara <gilbarbara@gmail.com>
2018-03-27 19:58:46 -0300

    add Beacon animation

--- src/components/Beacon.js
+++ src/components/Beacon.js
@@ -1,47 +1,98 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const JoyrideBeacon = ({ beaconComponent, onClickOrHover, styles }) => {
-  const props = {
-    'aria-label': 'Open',
-    onClick: onClickOrHover,
-    onMouseEnter: onClickOrHover,
-    title: 'Open',
-  };
-  let component;
+export default class JoyrideBeacon extends React.Component {
+  constructor(props) {
+    super(props);
 
-  if (beaconComponent) {
-    if (React.isValidElement(beaconComponent)) {
-      component = React.cloneElement(beaconComponent, props);
+    if (!props.beaconComponent) {
+      const head = document.head || document.getElementsByTagName('head')[0];
+      const style = document.createElement('style');
+      const css = `
+@keyframes joyride-beacon-inner {
+  20% {
+    opacity: 0.9;
+  }
+
+  90% {
+    opacity: 0.7;
+  }
+}
+
+@keyframes joyride-beacon-outer {
+  0% {
+    transform: scale(1);
+  }
+
+  45% {
+    opacity: 0.7;
+    transform: scale(0.75);
+  }
+
+  100% {
+    opacity: 0.9;
+    transform: scale(1);
+  }
+}
+      `;
+
+      style.type = 'text/css';
+      style.id = 'joyride-beacon-animation';
+      style.appendChild(document.createTextNode(css));
+
+      head.appendChild(style);
+    }
+  }
+
+  static propTypes = {
+    beaconComponent: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.element,
+    ]),
+    onClickOrHover: PropTypes.func.isRequired,
+    styles: PropTypes.object.isRequired,
+  };
+
+  componentWillUnmount() {
+    const style = document.getElementById('joyride-beacon-animation');
+
+    if (style) {
+      style.remove();
+    }
+  }
+
+  render() {
+    const { beaconComponent, onClickOrHover, styles } = this.props;
+    const props = {
+      'aria-label': 'Open',
+      onClick: onClickOrHover,
+      onMouseEnter: onClickOrHover,
+      title: 'Open',
+    };
+    let component;
+
+    if (beaconComponent) {
+      if (React.isValidElement(beaconComponent)) {
+        component = React.cloneElement(beaconComponent, props);
+      }
+      else {
+        component = beaconComponent(props);
+      }
     }
     else {
-      component = beaconComponent(props);
+      component = (
+        <button
+          key="JoyrideBeacon"
+          className="joyride-beacon"
+          style={styles.beacon}
+          {...props}
+        >
+          <span style={styles.beaconInner} />
+          <span style={styles.beaconOuter} />
+        </button>
+      );
     }
+
+    return component;
   }
-  else {
-    component = (
-      <button
-        key="JoyrideBeacon"
-        className="joyride-beacon"
-        style={styles.beacon}
-        {...props}
-      >
-        <span style={styles.beaconInner} />
-        <span style={styles.beaconOuter} />
-      </button>
-    );
-  }
-
-  return component;
-};
-
-JoyrideBeacon.propTypes = {
-  beaconComponent: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.element,
-  ]),
-  onClickOrHover: PropTypes.func.isRequired,
-  styles: PropTypes.object.isRequired,
-};
-
-export default JoyrideBeacon;
+}
