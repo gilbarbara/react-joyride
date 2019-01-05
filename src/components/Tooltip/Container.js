d5d9a708ed548eb95037cea606c6883d06acb945
Gil Barbara <gilbarbara@gmail.com>
2019-01-05 01:36:21 -0200

    Improve accessibility
    Fixes #460

--- src/components/Tooltip/Container.js
+++ src/components/Tooltip/Container.js
@@ -3,121 +3,120 @@ import PropTypes from 'prop-types';
 
 import CloseBtn from './CloseBtn';
 
-const JoyrideTooltipContainer = ({
-  continuous,
-  backProps,
-  closeProps,
-  primaryProps,
-  skipProps,
-  index,
-  isLastStep,
-  setTooltipRef,
-  size,
-  step,
-}) => {
-  const {
-    content,
-    hideBackButton,
-    hideCloseButton,
-    hideFooter,
-    locale,
-    showProgress,
-    showSkipButton,
-    title,
-    styles,
-  } = step;
-  const { back, close, last, next, skip } = locale;
-  const output = {
-    primary: close,
+export default class JoyrideTooltipContainer extends React.Component {
+  static propTypes = {
+    backProps: PropTypes.object.isRequired,
+    closeProps: PropTypes.object.isRequired,
+    continuous: PropTypes.bool.isRequired,
+    index: PropTypes.number.isRequired,
+    isLastStep: PropTypes.bool.isRequired,
+    primaryProps: PropTypes.object.isRequired,
+    size: PropTypes.number.isRequired,
+    skipProps: PropTypes.object.isRequired,
+    step: PropTypes.object.isRequired,
+    tooltipProps: PropTypes.object.isRequired,
   };
 
-  if (continuous) {
-    output.primary = isLastStep ? last : next;
+  render() {
+    const {
+      backProps,
+      closeProps,
+      continuous,
+      index,
+      isLastStep,
+      primaryProps,
+      size,
+      skipProps,
+      step,
+      tooltipProps,
+    } = this.props;
+    const {
+      content,
+      hideBackButton,
+      hideCloseButton,
+      hideFooter,
+      showProgress,
+      showSkipButton,
+      title,
+      styles,
+    } = step;
+    const { back, close, last, next, skip } = step.locale;
+    const output = {
+      primary: close,
+    };
 
-    if (showProgress) {
-      output.primary = <span>{output.primary} ({index + 1}/{size})</span>;
+    if (continuous) {
+      output.primary = isLastStep ? last : next;
+
+      if (showProgress) {
+        output.primary = <span>{output.primary} ({index + 1}/{size})</span>;
+      }
     }
-  }
 
-  if (showSkipButton && !isLastStep) {
-    output.skip = (
-      <button
-        style={styles.buttonSkip}
-        type="button"
-        data-test-id="button-skip"
-        {...skipProps}
-      >
-        {skip}
-      </button>
+    if (showSkipButton && !isLastStep) {
+      output.skip = (
+        <button
+          style={styles.buttonSkip}
+          type="button"
+          data-test-id="button-skip"
+          aria-live="off"
+          {...skipProps}
+        >
+          {skip}
+        </button>
+      );
+    }
+
+    if (!hideBackButton && index > 0) {
+      output.back = (
+        <button
+          style={styles.buttonBack}
+          type="button"
+          data-test-id="button-back"
+          {...backProps}
+        >
+          {back}
+        </button>
+      );
+    }
+
+    output.close = !hideCloseButton && (
+      <CloseBtn
+        styles={styles.buttonClose}
+        data-test-id="button-close"
+        {...closeProps}
+      />
     );
-  }
 
-  if (!hideBackButton && index > 0) {
-    output.back = (
-      <button
-        style={styles.buttonBack}
-        type="button"
-        data-test-id="button-back"
-        {...backProps}
+    return (
+      <div
+        key="JoyrideTooltip"
+        className="react-joyride__tooltip"
+        style={styles.tooltip}
+        {...tooltipProps}
       >
-        {back}
-      </button>
-    );
-  }
-
-  output.close = !hideCloseButton && (
-    <CloseBtn
-      styles={styles.buttonClose}
-      data-test-id="button-close"
-      {...closeProps}
-    />
-  );
-
-  return (
-    <div
-      key="JoyrideTooltip"
-      className="react-joyride__tooltip"
-      ref={setTooltipRef}
-      style={styles.tooltip}
-    >
-      <div style={styles.tooltipContainer}>
-        {output.close}
-        {title && (<h4 style={styles.tooltipTitle}>{title}</h4>)}
-        {!!content && (
+        <div style={styles.tooltipContainer}>
+          {title && (<h4 style={styles.tooltipTitle} aria-label={title}>{title}</h4>)}
           <div style={styles.tooltipContent}>
             {content}
           </div>
-        )}
-      </div>
-      {!hideFooter && (
-        <div style={styles.tooltipFooter}>
-          {output.skip}
-          {output.back}
-          <button
-            style={styles.buttonNext}
-            type="button"
-            data-test-id="button-primary"
-            {...primaryProps}
-          >
-            {output.primary}
-          </button>
         </div>
-      )}
-    </div>
-  );
-};
-
-JoyrideTooltipContainer.propTypes = {
-  backProps: PropTypes.object.isRequired,
-  closeProps: PropTypes.object.isRequired,
-  continuous: PropTypes.bool.isRequired,
-  index: PropTypes.number.isRequired,
-  isLastStep: PropTypes.bool.isRequired,
-  primaryProps: PropTypes.object.isRequired,
-  setTooltipRef: PropTypes.func.isRequired,
-  size: PropTypes.number.isRequired,
-  skipProps: PropTypes.object.isRequired,
-  step: PropTypes.object.isRequired,
-};
-
-export default JoyrideTooltipContainer;
+        {!hideFooter && (
+          <div style={styles.tooltipFooter} aria-relevant="additions">
+            {output.skip}
+            {output.back}
+            <button
+              style={styles.buttonNext}
+              type="button"
+              data-test-id="button-primary"
+              {...primaryProps}
+            >
+              {output.primary}
+            </button>
+          </div>
+        )}
+        {output.close}
+      </div>
+    );
+  }
+}
