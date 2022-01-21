b9c086b26077a39d4918cd8ea1fbc8a4e725260a
John Walker <mail@john-walker.co.uk>
2022-01-21 16:03:35 +0000

    Fix build

--- src/modules/scope.js
+++ src/modules/scope.js
@@ -48,7 +48,7 @@ export default class Scope {
     }
   };
 
-interceptTab = (event: KeyboardEvent) => {
+  interceptTab = (event: KeyboardEvent) => {
     const elements = this.findValidTabElements();
 
     if (!elements.length) {
