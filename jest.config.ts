2fe4237dcad4477ab3a0fd0085efccf7fca591bb
Gil Barbara <gilbarbara@gmail.com>
2023-10-23 11:51:51 -0300

    Upgrade dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,10 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   coverageThreshold: {
     global: {
-      branches: 15,
-      functions: 15,
-      lines: 15,
-      statements: 15,
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
   moduleDirectories: ['node_modules', 'src'],
