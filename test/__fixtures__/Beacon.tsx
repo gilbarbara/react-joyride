317127c8fdfe70aaba9e0a5a9caa6e98cf51d7f1
Gil Barbara <gilbarbara@gmail.com>
2023-12-12 11:12:44 -0300

    Upgrade dependencies
    - fix lint warnings

--- test/__fixtures__/Beacon.tsx
+++ test/__fixtures__/Beacon.tsx
@@ -4,7 +4,13 @@ import { BeaconRenderProps } from '~/types';
 
 const BeaconComponent = forwardRef<HTMLButtonElement, BeaconRenderProps>((_, ref) => {
   return (
-    <button ref={ref} className="react-joyride__beacon" data-test-id="button-beacon" type="button">
+    <button
+      ref={ref}
+      aria-label="Beacon"
+      className="react-joyride__beacon"
+      data-test-id="button-beacon"
+      type="button"
+    >
       <span className="react-joyride__beacon__inner" />
     </button>
   );
