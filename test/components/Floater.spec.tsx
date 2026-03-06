f5c8ef74ffbee43e859059bf1fb346a5a3c21c38
Gil Barbara <gilbarbara@gmail.com>
2026-03-06 17:03:08 -0300

    Optimize Floater reactive changes

--- test/components/Floater.spec.tsx
+++ test/components/Floater.spec.tsx
@@ -399,7 +399,12 @@ describe('Floater', () => {
 
       render(<Floater {...props} />);
 
-      expect(offsetMock).toHaveBeenCalledWith(expect.any(Function));
+      expect(offsetMock).toHaveBeenCalledWith(expect.any(Function), [
+        10,
+        { bottom: 5, left: 5, right: 5, top: 5 },
+        16,
+        true,
+      ]);
 
       const offsetFn = (offsetMock as ReturnType<typeof vi.fn>).mock.calls[0][0] as (state: {
         placement: string;
